3251e773b8522ca9ac4743faf71bba903809cc05
tricticle <tricticle@gmail.com>
2023-09-20 00:28:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,6 +38,12 @@ app.use(bodyParser.json());
 
 // Define routes to add, remove, and fetch data
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next()
+});
+
 // Add data to MongoDB
 app.post('/addData', async (req, res) => {
   try {
