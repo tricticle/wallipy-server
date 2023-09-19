f40e10ce9905ee7edd63ca7afeb4bf7f4ff9b985
tricticle <tricticle@gmail.com>
2023-09-19 21:58:33 +0530

    Revert "added changes"
    This reverts commit a89844f09a2ab1ec68d643322df4193135873c60.

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cors = require('cors'); 
 require('dotenv').config()
 
 const app = express();
 const port = process.env.PORT || 5000;
-
-app.use(cors());
 
 // Connect to your MongoDB database
 mongoose.connect('mongodb+srv://tricticle:_____vighnesh@cluster0.ynsd50o.mongodb.net/wallipy', { useNewUrlParser: true, useUnifiedTopology: true })
