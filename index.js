2702e297d588839e42fbe28e15b9d5e448826df7
tricticle <tricticle@gmail.com>
2023-09-19 21:49:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 // Connect to your MongoDB database
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://tricticle:_____vighnesh@cluster0.ynsd50o.mongodb.net/wallipy', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
   })
