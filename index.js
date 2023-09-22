c5cde560558babce9a18537057c967fba87d9783
tricticle <tricticle@gmail.com>
2023-09-23 00:39:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,27 +9,6 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-// Connect to your MongoDB database
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
-
-// Create a Mongoose schema and model for your data
-const imageSchema = new mongoose.Schema({
-  title: String,
-  imageUrl: { type: String, unique: true }, // Make imageUrl field unique
-  description: String,
-});
-
-const Image = mongoose.model('Image', imageSchema);
-
-app.use(bodyParser.json());
-
-// Define routes to add, remove, and fetch data
 
 app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://wallipy.art/");
@@ -43,19 +22,61 @@ app.all('/', function(req, res, next) {
   next()
 });
 
+
+
+// Connect to your MongoDB database
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+// Create a Mongoose schema and model for your data
+const imageSchema = new mongoose.Schema({
+  username: String,
+  data: [
+    {
+      title: String,
+      imageUrl: { type: String, unique: true },
+      description: String,
+    },
+  ],
+});
+
+const Image = mongoose.model('Image', imageSchema);
+
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
-  res.send('😁    Welcome to wallipyServer    😁')
   res.set('Access-Control-Allow-Origin', '*');
-})
+  res.send('😁    Welcome to wallipyServer    😁');
+});
 
 // Add data to MongoDB
 app.post('/addData', async (req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
   try {
-    const { title, imageUrl, description } = req.body;
-    const newImage = new Image({ title, imageUrl, description });
-    await newImage.save();
-    res.status(201).json(newImage);
+    const { title, imageUrl, description, username } = req.body;
+
+    // Check if the user already exists in the database
+    let userImage = await Image.findOne({ username });
+
+    if (!userImage) {
+      // If the user doesn't exist, create a new user document
+      userImage = new Image({
+        username,
+        data: [],
+      });
+    }
+
+    // Add the new data to the user's data array
+    userImage.data.push({ title, imageUrl, description });
+
+    // Save the updated user document
+    await userImage.save();
+    res.status(201).json(userImage.data);
   } catch (error) {
     res.status(500).json({ error: 'Error adding data to MongoDB' });
   }
@@ -65,23 +86,41 @@ app.post('/addData', async (req, res) => {
 app.delete('/removeData', async (req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
   try {
-    const { imageUrl } = req.body;
-    const image = await Image.findOneAndRemove({ imageUrl });
-    if (!image) {
-      return res.status(404).json({ error: 'Image not found' });
+    const { imageUrl, username } = req.body;
+
+    // Find the user by username
+    const userImage = await Image.findOne({ username });
+
+    if (!userImage) {
+      return res.status(404).json({ error: 'User not found' });
     }
+
+    // Remove the image with the matching imageUrl from the user's data array
+    userImage.data = userImage.data.filter((image) => image.imageUrl !== imageUrl);
+
+    // Save the updated user document
+    await userImage.save();
+
     res.json({ message: 'Image removed successfully' });
   } catch (error) {
     res.status(500).json({ error: 'Error removing data from MongoDB' });
   }
 });
 
-// Fetch all added data from MongoDB
+// Fetch all added data for a user from MongoDB
 app.get('/addedData', async (req, res) => {
   res.set('Access-Control-Allow-Origin', '*');
   try {
-    const addedData = await Image.find();
-    res.json(addedData);
+    const { username } = req.query;
+
+    // Find the user by username and return their data array
+    const userImage = await Image.findOne({ username });
+
+    if (!userImage) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    res.json(userImage.data);
   } catch (error) {
     res.status(500).json({ error: 'Error fetching added data from MongoDB' });
   }
